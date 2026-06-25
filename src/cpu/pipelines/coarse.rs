94c8108b0aa22660841a17b2e95385701cb8e072
JianJian Li <ljj1849532909@gmail.com>
2026-06-24 22:56:13 -0400

    a

--- src/cpu/pipelines/coarse.rs
+++ src/cpu/pipelines/coarse.rs
@@ -1,12 +1,8 @@
 use crate::{
     cpu::computes::coarse::rasterize_tile,
     shared::{
-        bd_record::BackdropRecord,
-        bounds::TileBbox,
-        draw_record::DrawRecord,
-        image::Image,
-        line_seg::LineSegment,
-        tile_seg_range::TileSegmentRange,
+        bd_record::BackdropRecord, bounds::TileBbox, draw_record::DrawRecord, image::Image,
+        line_seg::LineSegment, tile_seg_range::TileSegmentRange,
     },
 };
 
@@ -28,37 +24,33 @@ impl<'a> CoarseCpuPrepared<'a> {
             };
             let backdrop_record = &self.backdrop_records[path_id as usize];
             let bbox = draw.tile_bbox(self.tiles_size.0, self.tiles_size.1);
-            self.rasterize_draw(draw, backdrop_record, bbox);
-        }
-    }
+            let stride = backdrop_record.tile_x1 - backdrop_record.tile_x0;
+            if stride == 0 {
+                return;
+            }
+            for tile_y in bbox.y0..bbox.y1 {
+                for tile_x in bbox.x0..bbox.x1 {
+                    let local_x = tile_x - backdrop_record.tile_x0;
+                    let local_y = tile_y - backdrop_record.tile_y0;
+                    let local_ix = (local_y * stride + local_x) as usize;
+                    let backdrop_ix = backdrop_record.data_offset as usize + local_ix;
+                    let segment_range = self.tile_segment_ranges[backdrop_ix];
+                    let segments =
+                        &self.segments[segment_range.start as usize..segment_range.end as usize];
+                    let backdrop = self.backdrops[backdrop_ix];
 
-    fn rasterize_draw(&mut self, draw: &DrawRecord, backdrop_record: &BackdropRecord, bbox: TileBbox) {
-        let stride = backdrop_record.tile_x1 - backdrop_record.tile_x0;
-        if stride == 0 {
-            return;
-        }
-
-        for tile_y in bbox.y0..bbox.y1 {
-            for tile_x in bbox.x0..bbox.x1 {
-                let local_x = tile_x - backdrop_record.tile_x0;
-                let local_y = tile_y - backdrop_record.tile_y0;
-                let local_ix = (local_y * stride + local_x) as usize;
-                let backdrop_ix = backdrop_record.data_offset as usize + local_ix;
-                let segment_range = self.tile_segment_ranges[backdrop_ix];
-                let segments = &self.segments[segment_range.start as usize..segment_range.end as usize];
-                let backdrop = self.backdrops[backdrop_ix];
-
-                rasterize_tile(
-                    &mut self.target.pixels,
-                    self.target.width,
-                    self.target.height,
-                    tile_x,
-                    tile_y,
-                    segments,
-                    backdrop,
-                    draw.fill_rule,
-                    &draw.brush,
-                );
+                    rasterize_tile(
+                        &mut self.target.pixels,
+                        self.target.width,
+                        self.target.height,
+                        tile_x,
+                        tile_y,
+                        segments,
+                        backdrop,
+                        draw.fill_rule,
+                        &draw.brush,
+                    );
+                }
             }
         }
     }
